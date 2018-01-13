19c8f47f7003bed3abdc42d9afa5d755a6422901
Su-Shing Chen <su-shing_chen@hotmail.co.nz>
2018-01-13 22:54:50 +1300

    Refactor to get MAC address bytes first. Test the actual returned MAC address.

--- wakeonlan/src/main/java/com/github/shingyx/wakeonlan/MagicPacketSender.java
+++ wakeonlan/src/main/java/com/github/shingyx/wakeonlan/MagicPacketSender.java
@@ -20,26 +20,37 @@ public class MagicPacketSender {
     private static final int WOL_PORT = 9;
 
     public void send(Context context, String macAddress) throws Exception {
-        if (!isValidMacAddress(macAddress)) {
-            throw new IllegalArgumentException("Invalid MAC address");
-        }
-        byte[] bytes = getMagicPacketBytes(macAddress);
+        byte[] macAddressBytes = convertMacAddressString(macAddress);
+        byte[] packetBytes = getMagicPacketBytes(macAddressBytes);
         InetAddress ip = getBroadcastAddress(context);
-        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, ip, WOL_PORT);
+        DatagramPacket packet = new DatagramPacket(packetBytes, packetBytes.length, ip, WOL_PORT);
         try (DatagramSocket socket = new DatagramSocket()) {
             socket.send(packet);
         }
     }
 
-    public boolean isValidMacAddress(String macAddress) throws IllegalArgumentException {
-        if (macAddress == null || macAddress.isEmpty()) {
+    public byte[] convertMacAddressString(String macAddress) {
+        if (!isValidMacAddress(macAddress)) {
+            throw new IllegalArgumentException("Invalid MAC address");
+        }
+        String[] parts = macAddress.split("[:-]");
+        byte[] bytes = new byte[MAC_ADDRESS_BYTE_LENGTH];
+        for (int i = 0; i < MAC_ADDRESS_BYTE_LENGTH; i++) {
+            int hex = Integer.parseInt(parts[i], 16);
+            bytes[i] = (byte) hex;
+        }
+        return bytes;
+    }
+
+    private boolean isValidMacAddress(String macAddress) throws IllegalArgumentException {
+        if (macAddress == null) {
             return false;
         }
         Pattern pattern = Pattern.compile("([0-9a-f]{2}[:-]){5}[0-9a-f]{2}", Pattern.CASE_INSENSITIVE);
         return pattern.matcher(macAddress).matches();
     }
 
-    private byte[] getMagicPacketBytes(String macAddress) {
+    private byte[] getMagicPacketBytes(byte[] macAddressBytes) {
         byte[] packet = new byte[MAGIC_PACKET_LENGTH];
 
         // Synchronization Stream
@@ -48,22 +59,11 @@ public class MagicPacketSender {
         }
 
         // Target MAC copied 16 times
-        byte[] macAddressBytes = convertMacAddressString(macAddress);
         for (int i = SYNC_STREAM_LENGTH; i < MAGIC_PACKET_LENGTH; i += MAC_ADDRESS_BYTE_LENGTH) {
             System.arraycopy(macAddressBytes, 0, packet, i, MAC_ADDRESS_BYTE_LENGTH);
         }
 
         return packet;
-    }
-
-    private byte[] convertMacAddressString(String macAddress) {
-        String[] parts = macAddress.split("[:-]");
-        byte[] bytes = new byte[MAC_ADDRESS_BYTE_LENGTH];
-        for (int i = 0; i < MAC_ADDRESS_BYTE_LENGTH; i++) {
-            int hex = Integer.parseInt(parts[i], 16);
-            bytes[i] = (byte) hex;
-        }
-        return bytes;
     }
 
     private InetAddress getBroadcastAddress(Context context) throws UnknownHostException {
--- wakeonlan/src/test/java/com/github/shingyx/wakeonlan/MagicPacketSenderTest.java
+++ wakeonlan/src/test/java/com/github/shingyx/wakeonlan/MagicPacketSenderTest.java
@@ -15,57 +15,88 @@ public class MagicPacketSenderTest {
     }
 
     @Test
-    public void isValidMacAddress_allZero() throws Exception {
-        assertTrue(instance.isValidMacAddress("00:00:00:00:00:00"));
+    public void convertMacAddressString_allZero() throws Exception {
+        runHappyTest(new byte[]{0, 0, 0, 0, 0, 0}, "00:00:00:00:00:00");
     }
 
     @Test
-    public void isValidMacAddress_allF() throws Exception {
-        assertTrue(instance.isValidMacAddress("ff:ff:ff:ff:ff:ff"));
+    public void convertMacAddressString_allF() throws Exception {
+        runHappyTest(new int[]{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, "ff:ff:ff:ff:ff:ff");
     }
 
     @Test
-    public void isValidMacAddress_aToF() throws Exception {
-        assertTrue(instance.isValidMacAddress("ab:cd:ef:fe:dc:ba"));
+    public void convertMacAddressString_aToF() throws Exception {
+        runHappyTest(new int[]{0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba}, "ab:cd:ef:fe:dc:ba");
     }
 
     @Test
-    public void isValidMacAddress_upperCase() throws Exception {
-        assertTrue(instance.isValidMacAddress("FF:FF:FF:FF:FF:FF"));
+    public void convertMacAddressString_upperCase() throws Exception {
+        runHappyTest(new int[]{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, "FF:FF:FF:FF:FF:FF");
     }
 
     @Test
-    public void isValidMacAddress_hyphenSeparator() throws Exception {
-        assertTrue(instance.isValidMacAddress("00-00-00-00-00-00"));
+    public void convertMacAddressString_hyphenSeparator() throws Exception {
+        runHappyTest(new byte[]{0, 0, 0, 0, 0, 0}, "00-00-00-00-00-00");
     }
 
     @Test
-    public void isValidMacAddress_containsInvalidHex() throws Exception {
-        assertFalse(instance.isValidMacAddress("00:00:00:00:00:0g"));
+    public void convertMacAddressString_containsInvalidHex() throws Exception {
+        runSadTest("00:00:00:00:00:0g");
     }
 
     @Test
-    public void isValidMacAddress_tooShort() throws Exception {
-        assertFalse(instance.isValidMacAddress("ff:ff:ff:ff:ff:f"));
+    public void convertMacAddressString_tooShort() throws Exception {
+        runSadTest("ff:ff:ff:ff:ff:f");
     }
 
     @Test
-    public void isValidMacAddress_tooLong() throws Exception {
-        assertFalse(instance.isValidMacAddress("ff:ff:ff:ff:ff:fff"));
+    public void convertMacAddressString_tooLong() throws Exception {
+        runSadTest("ff:ff:ff:ff:ff:fff");
     }
 
     @Test
-    public void isValidMacAddress_doubleSeparator() throws Exception {
-        assertFalse(instance.isValidMacAddress("ff:ff:ff:ff:ff::f"));
+    public void convertMacAddressString_tooLong2() throws Exception {
+        runSadTest("ff:ff:ff:ff:ff:ff-");
     }
 
     @Test
-    public void isValidMacAddress_noSeparators() throws Exception {
-        assertFalse(instance.isValidMacAddress("ffffffffffff"));
+    public void convertMacAddressString_doubleSeparator() throws Exception {
+        runSadTest("ff:ff:ff:ff:ff::f");
     }
 
     @Test
-    public void isValidMacAddress_null() throws Exception {
-        assertFalse(instance.isValidMacAddress(null));
+    public void convertMacAddressString_noSeparators() throws Exception {
+        runSadTest("ffffffffffff");
+    }
+
+    @Test
+    public void convertMacAddressString_emptyString() throws Exception {
+        runSadTest("");
+    }
+
+    @Test
+    public void convertMacAddressString_null() throws Exception {
+        runSadTest(null);
+    }
+
+    private void runHappyTest(byte[] expected, String input) {
+        assertArrayEquals(expected, instance.convertMacAddressString(input));
+    }
+
+    private void runHappyTest(int[] expected, String input) {
+        byte[] expectedBytes = new byte[expected.length];
+        for (int i = 0; i < expected.length; i++) {
+            expectedBytes[i] = (byte) expected[i];
+        }
+        runHappyTest(expectedBytes, input);
+    }
+
+    private void runSadTest(String input) {
+        try {
+            instance.convertMacAddressString(input);
+            fail("Expected an exception");
+        } catch (IllegalArgumentException e) {
+            // Caught an IllegalArgumentException
+        }
     }
 }
