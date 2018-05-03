e8ba01a433b9b297af04c4984290358c32a4b15d
Su-Shing Chen <Shingyx@users.noreply.github.com>
2018-05-03 22:21:46 +1200

    Support JUnit 5 and remove androidTest dependencies until I need them

--- wakeonlan/src/test/java/com/github/shingyx/wakeonlan/MagicPacketProcessorTest.kt
+++ wakeonlan/src/test/java/com/github/shingyx/wakeonlan/MagicPacketProcessorTest.kt
@@ -1,82 +1,82 @@
 package com.github.shingyx.wakeonlan
 
-import org.junit.Assert.assertArrayEquals
-import org.junit.Assert.fail
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 
 class MagicPacketProcessorTest {
 
-    @Test
-    fun convertMacAddressString_allZero() {
-        runHappyTest(intArrayOf(0, 0, 0, 0, 0, 0), "00:00:00:00:00:00")
-    }
+    @Nested
+    inner class ConvertMacAddressString {
 
-    @Test
-    fun convertMacAddressString_allF() {
-        runHappyTest(intArrayOf(0xff, 0xff, 0xff, 0xff, 0xff, 0xff), "ff:ff:ff:ff:ff:ff")
-    }
+        @Test
+        fun allZero() {
+            runHappyTest(intArrayOf(0, 0, 0, 0, 0, 0), "00:00:00:00:00:00")
+        }
 
-    @Test
-    fun convertMacAddressString_aToF() {
-        runHappyTest(intArrayOf(0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba), "ab:cd:ef:fe:dc:ba")
-    }
+        @Test
+        fun allF() {
+            runHappyTest(intArrayOf(0xff, 0xff, 0xff, 0xff, 0xff, 0xff), "ff:ff:ff:ff:ff:ff")
+        }
 
-    @Test
-    fun convertMacAddressString_upperCase() {
-        runHappyTest(intArrayOf(0xff, 0xff, 0xff, 0xff, 0xff, 0xff), "FF:FF:FF:FF:FF:FF")
-    }
+        @Test
+        fun aToF() {
+            runHappyTest(intArrayOf(0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba), "ab:cd:ef:fe:dc:ba")
+        }
 
-    @Test
-    fun convertMacAddressString_hyphenSeparator() {
-        runHappyTest(intArrayOf(0, 0, 0, 0, 0, 0), "00-00-00-00-00-00")
-    }
+        @Test
+        fun upperCase() {
+            runHappyTest(intArrayOf(0xff, 0xff, 0xff, 0xff, 0xff, 0xff), "FF:FF:FF:FF:FF:FF")
+        }
 
-    @Test
-    fun convertMacAddressString_containsInvalidHex() {
-        runSadTest("00:00:00:00:00:0g")
-    }
+        @Test
+        fun hyphenSeparator() {
+            runHappyTest(intArrayOf(0, 0, 0, 0, 0, 0), "00-00-00-00-00-00")
+        }
 
-    @Test
-    fun convertMacAddressString_tooShort() {
-        runSadTest("ff:ff:ff:ff:ff:f")
-    }
+        @Test
+        fun containsInvalidHex() {
+            runSadTest("00:00:00:00:00:0g")
+        }
 
-    @Test
-    fun convertMacAddressString_tooLong() {
-        runSadTest("ff:ff:ff:ff:ff:fff")
-    }
+        @Test
+        fun tooShort() {
+            runSadTest("ff:ff:ff:ff:ff:f")
+        }
 
-    @Test
-    fun convertMacAddressString_tooLong2() {
-        runSadTest("ff:ff:ff:ff:ff:ff-")
-    }
+        @Test
+        fun tooLong() {
+            runSadTest("ff:ff:ff:ff:ff:fff")
+        }
 
-    @Test
-    fun convertMacAddressString_doubleSeparator() {
-        runSadTest("ff:ff:ff:ff:ff::f")
-    }
+        @Test
+        fun tooLong2() {
+            runSadTest("ff:ff:ff:ff:ff:ff-")
+        }
 
-    @Test
-    fun convertMacAddressString_noSeparators() {
-        runSadTest("ffffffffffff")
-    }
+        @Test
+        fun doubleSeparator() {
+            runSadTest("ff:ff:ff:ff:ff::f")
+        }
 
-    @Test
-    fun convertMacAddressString_emptyString() {
-        runSadTest("")
-    }
+        @Test
+        fun noSeparators() {
+            runSadTest("ffffffffffff")
+        }
 
-    private fun runHappyTest(expected: IntArray, input: String) {
-        val expectedBytes = expected.map { it.toByte() }.toByteArray()
-        assertArrayEquals(expectedBytes, convertMacAddressString(input))
-    }
+        @Test
+        fun emptyString() {
+            runSadTest("")
+        }
 
-    private fun runSadTest(input: String) {
-        try {
-            convertMacAddressString(input)
-            fail("Expected an exception")
-        } catch (e: IllegalArgumentException) {
-            // Caught an IllegalArgumentException
+        private fun runHappyTest(expected: IntArray, input: String) {
+            val expectedBytes = expected.map { it.toByte() }.toByteArray()
+            assertArrayEquals(expectedBytes, convertMacAddressString(input))
+        }
+
+        private fun runSadTest(input: String) {
+            assertThrows(IllegalArgumentException::class.java, { convertMacAddressString(input) })
         }
     }
 }
