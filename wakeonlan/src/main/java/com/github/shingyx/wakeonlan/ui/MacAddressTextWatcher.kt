13deaee4bfc467f844ba0e4eb06db4c634605707
Su-Shing Chen <shingyx.dev@gmail.com>
2024-03-31 13:18:09 +1100

    Insert trailing colons to avoid cursor jumps (#3)
    * Insert trailing colons when needed
    
    * .
    
    * rename

--- wakeonlan/src/main/java/com/github/shingyx/wakeonlan/ui/MacAddressTextWatcher.kt
+++ wakeonlan/src/main/java/com/github/shingyx/wakeonlan/ui/MacAddressTextWatcher.kt
@@ -23,18 +23,26 @@ class MacAddressTextWatcher : TextWatcher {
     private fun formatMacAddress(text: String): String {
         val builder = StringBuilder()
         var digits = 0
+        var insertTrailingColon = false
         for (char in text) {
             if (char.isLetterOrDigit()) {
-                if (digits % 2 == 0 && digits > 0 && digits < 12) {
+                if (allowSeparator(digits)) {
                     builder.append(':')
+                    insertTrailingColon = false
                 }
                 builder.append(char)
                 digits++
+            } else if (char == ':') {
+                insertTrailingColon = true
             }
         }
-        if (text.lastOrNull() == ':') {
+        if ((insertTrailingColon || text.lastOrNull() == ':') && allowSeparator(digits)) {
             builder.append(':')
         }
         return builder.toString()
+    }
+
+    private fun allowSeparator(digits: Int): Boolean {
+        return digits % 2 == 0 && digits in 1 until 12
     }
 }
